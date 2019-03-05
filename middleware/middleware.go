e471a4180623dd6caacff11910b1d6111fc532ec
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-05 19:43:04 +0900

    Add cache control

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,8 +5,16 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+func CacheControlHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Cache-Control", "public, max-age=3600, s-maxage=3600")
+		return next(c)
+	}
+}
+
 func Set(e *echo.Echo) {
 	// Set Bundle MiddleWare
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
+	e.Use(CacheControlHeader)
 }
