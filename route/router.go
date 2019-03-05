fbcc041ce48a2a47195e49f848a486a043d2e1e8
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-06 02:37:47 +0900

    Improve route

--- route/router.go
+++ route/router.go
@@ -17,12 +17,21 @@ func Init() *echo.Echo {
 	// Routes
 	packagesRoute := e.Group("/packages")
 	{
-		packagesRoute.GET("/:name/:version/deps", packages.Deps())
-		packagesRoute.GET("/:org/:name/:version/deps", packages.DepsOrg())
-		packagesRoute.GET("/:name/versions", packages.Versions())
-		packagesRoute.GET("/:org/:name/versions", packages.VersionsOrg())
-		packagesRoute.GET("/:name/:version/exists", packages.Exists())
-		packagesRoute.GET("/:org/:name/:version/exists", packages.ExistsOrg())
+		depsRoute := packagesRoute.Group("/deps")
+		{
+			depsRoute.GET("/:name/:version", packages.Deps())
+			depsRoute.GET("/:org/:name/:version", packages.DepsOrg())
+		}
+		versionsRoute := packagesRoute.Group("/versions")
+		{
+			versionsRoute.GET("/:name", packages.Versions())
+			versionsRoute.GET("/:org/:name", packages.VersionsOrg())
+		}
+		existsRoute := packagesRoute.Group("/exists")
+		{
+			existsRoute.GET("/:name/:version", packages.Exists())
+			existsRoute.GET("/:org/:name/:version", packages.ExistsOrg())
+		}
 		packagesRoute.POST("/upload", packages.Upload())
 	}
 	e.POST("/tokens/validate", tokens.Validate())
