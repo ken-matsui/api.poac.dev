dd681f0e456ce5351c70265aeb1150d4b77f176b
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-06 17:18:38 +0900

    Improve archive api

--- api/packages/archive.go
+++ api/packages/archive.go
@@ -1,43 +1,42 @@
 package packages
 
 import (
+	"errors"
 	"github.com/blang/semver"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
+func archiveUrl(name string, version string) (string, error) {
+	url := "https://storage.googleapis.com/poac-pm.appspot.com/"
+	url += name // TODO: upload.goで使用している検証をこちらでも行う
+
+	_, err := semver.Make(version) // semver error
+	if err != nil {
+		errStr := "Invalid version.\nPlease adapt to semver.\nSee https://semver.org for details."
+		return "", errors.New(errStr)
+	}
+	url += "-" + version
+	url += ".tar.gz"
+	return url, nil
+}
+
 func Archive() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		url := "https://storage.googleapis.com/poac-pm.appspot.com/"
-		url += c.Param("name") // TODO: upload.goで使用している検証をこちらでも行う
-
-		version := c.Param("version")
-		_, err := semver.Make(version) // semver error
+		url, err := archiveUrl(c.Param("name"), c.Param("version"))
 		if err != nil {
-			errStr := "Invalid version.\nPlease adapt to semver.\nSee https://semver.org for details."
-			return echo.NewHTTPError(http.StatusInternalServerError, errStr)
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
-
-		url += "-" + version
-		url += ".tar.gz"
 		return c.Redirect(http.StatusPermanentRedirect, url)
 	}
 }
 
 func ArchiveDeps() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		url := "https://storage.googleapis.com/poac-pm.appspot.com/"
-		url += c.Param("org") + "-" + c.Param("name")
-
-		version := c.Param("version")
-		_, err := semver.Make(version) // semver error
+		url, err := archiveUrl(c.Param("org") + "-" + c.Param("name"), c.Param("version"))
 		if err != nil {
-			errStr := "Invalid version.\nPlease adapt to semver.\nSee https://semver.org for details."
-			return echo.NewHTTPError(http.StatusInternalServerError, errStr)
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
-
-		url += "-" + version
-		url += ".tar.gz"
 		return c.Redirect(http.StatusPermanentRedirect, url)
 	}
 }
