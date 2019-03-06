762d6d51225a848f2044cd8a67709c5e144339d3
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2019-03-07 04:34:14 +0900

    Add test

---
+++ test/misc/check_test.go
@@ -0,0 +1,22 @@
+// +build !appengine
+
+package main
+
+import (
+	"github.com/poacpm/api.poac.pm/misc"
+	"testing"
+)
+
+func TestCheckPackageName1(t *testing.T) {
+	err := misc.CheckPackageName("hoge")
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+}
+
+func TestCheckPackageName2(t *testing.T) {
+	err := misc.CheckPackageName("*hoge")
+	if err == nil {
+		t.Fatal("failed test")
+	}
+}
