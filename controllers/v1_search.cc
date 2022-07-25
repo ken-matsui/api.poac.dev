2ccc2cc0b22e08f4ed328db7754aea0126daab5e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-25 20:01:12 +0900

    Create search API

---
+++ controllers/v1_search.cc
@@ -0,0 +1,49 @@
+#include "controllers/v1_search.h"
+
+// std
+#include <string>
+
+// internal
+#include "models/Packages.h"
+
+// external
+#include <drogon/orm/Mapper.h>
+
+using drogon_model::postgres::Packages;
+
+void
+v1::search::asyncHandleHttpRequest(
+    const drogon::HttpRequestPtr& req,
+    std::function<void(const drogon::HttpResponsePtr&)>&& callback
+) {
+  const auto request = req->getJsonObject();
+  if (request) {
+    if (const Json::Value query = request->get("query", ""); query.isString()) {
+      const drogon::orm::DbClientPtr clientPtr = drogon::app().getDbClient();
+      drogon::orm::Mapper<Packages> mp(clientPtr);
+      const std::vector<Packages> packages = mp.findBy(drogon::orm::Criteria(
+          Packages::Cols::_name, drogon::orm::CompareOperator::Like,
+          "%" + query.asString() + "%"
+      ));
+
+      Json::Value packages_j(Json::arrayValue);
+      for (const Packages& row : packages) {
+        packages_j.append(row.toJson());
+      }
+
+      const drogon::HttpResponsePtr resp =
+          drogon::HttpResponse::newHttpJsonResponse(packages_j);
+      resp->setStatusCode(drogon::k200OK);
+      resp->setContentTypeCode(drogon::CT_APPLICATION_JSON);
+      callback(resp);
+
+      return;
+    }
+  }
+
+  const drogon::HttpResponsePtr resp =
+      drogon::HttpResponse::newHttpJsonResponse(Json::Value());
+  resp->setStatusCode(drogon::k200OK);
+  resp->setContentTypeCode(drogon::CT_APPLICATION_JSON);
+  callback(resp);
+}
---
+++ controllers/v1_search.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <drogon/HttpSimpleController.h>
+
+namespace v1 {
+class search : public drogon::HttpSimpleController<search> {
+public:
+  void
+  asyncHandleHttpRequest(
+      const drogon::HttpRequestPtr& req,
+      std::function<void(const drogon::HttpResponsePtr&)>&& callback
+  ) override;
+  PATH_LIST_BEGIN
+
+  PATH_ADD("/v1/search", drogon::Post);
+
+  PATH_LIST_END
+};
+} // namespace v1
