1143b2fa06f7bd4a227706bf056b3f4ccd815f71
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-27 19:21:47 +0900

    Implement perPage

--- controllers/v1.cc
+++ controllers/v1.cc
@@ -14,28 +14,48 @@
 
 using drogon_model::postgres::Package;
 
-// TODO(ken-matsui): Support `perPage`
 void
 v1::search(
     const HttpRequestPtr& req,
     std::function<void(const HttpResponsePtr&)>&& callback
 ) {
   const auto request = req->getJsonObject();
-  if (const Json::Value query = request->get("query", ""); query.isString()) {
-    drogon::orm::Mapper<Package> mp(drogon::app().getDbClient());
-    const std::vector<Package> result = mp.findBy(drogon::orm::Criteria(
+
+  // Validations
+  const Json::Value query = request->get("query", "");
+  if (!query.isString()) {
+    callback(poac_api::badRequest("`query` must be string"));
+    return;
+  }
+  const Json::Value perPage = request->get("perPage", 0);
+  if (!perPage.isUInt()) {
+    callback(poac_api::badRequest("`perPage` must be unsigned int"));
+    return;
+  }
+
+  // SQL query
+  drogon::orm::Mapper<Package> mp(drogon::app().getDbClient());
+  std::vector<Package> result;
+  if (perPage.asUInt() == 0) {
+    result = mp.findBy(drogon::orm::Criteria(
         Package::Cols::_name, drogon::orm::CompareOperator::Like,
         "%" + query.asString() + "%"
     ));
-
-    Json::Value packages(Json::arrayValue);
-    for (const Package& row : result) {
-      packages.append(row.toJson());
-    }
-    callback(poac_api::ok(packages));
   } else {
-    callback(poac_api::badRequest("`query` must be string"));
+    result = mp.offset(0)
+                 .limit(perPage.asUInt() + 1)
+                 .findBy(drogon::orm::Criteria(
+                     Package::Cols::_name, drogon::orm::CompareOperator::Like,
+                     "%" + query.asString() + "%"
+                 ));
   }
+
+  // Response build
+  Json::Value packages(Json::arrayValue);
+  for (const Package& row : result) {
+    packages.append(row.toJson());
+  }
+  callback(poac_api::ok(packages));
 }
 
 void
@@ -44,25 +64,30 @@ v1::versions(
     std::function<void(const HttpResponsePtr&)>&& callback
 ) {
   const auto request = req->getJsonObject();
-  if (const Json::Value name = request->get("name", ""); name.isString()) {
-    const drogon::orm::DbClientPtr clientPtr = drogon::app().getDbClient();
 
-    try {
-      const auto result = clientPtr->execSqlSync(
-          "select version from package where name = $1", name.asString()
-      );
-
-      Json::Value versions(Json::arrayValue);
-      for (const drogon::orm::Row& row : result) {
-        versions.append(row["version"].as<std::string>());
-      }
-      callback(poac_api::ok(versions));
-    } catch (const drogon::orm::DrogonDbException& e) {
-      LOG_ERROR << e.base().what();
-      callback(poac_api::internalError());
-    }
-  } else {
+  // Validations
+  const Json::Value name = request->get("name", "");
+  if (!name.isString()) {
     callback(poac_api::badRequest("`name` must be string"));
+    return;
+  }
+
+  // SQL query
+  const drogon::orm::DbClientPtr clientPtr = drogon::app().getDbClient();
+  try {
+    const auto result = clientPtr->execSqlSync(
+        "select version from package where name = $1", name.asString()
+    );
+
+    // Response build
+    Json::Value versions(Json::arrayValue);
+    for (const drogon::orm::Row& row : result) {
+      versions.append(row["version"].as<std::string>());
+    }
+    callback(poac_api::ok(versions));
+  } catch (const drogon::orm::DrogonDbException& e) {
+    LOG_ERROR << e.base().what();
+    callback(poac_api::internalError());
   }
 }
 
@@ -72,31 +97,34 @@ v1::repoinfo(
     std::function<void(const HttpResponsePtr&)>&& callback
 ) {
   const auto request = req->getJsonObject();
+
+  // Validations
   const Json::Value name = request->get("name", "");
   const Json::Value version = request->get("version", "");
-
-  if (name.isString() && version.isString()) {
-    const drogon::orm::DbClientPtr clientPtr = drogon::app().getDbClient();
-
-    try {
-      const auto result = clientPtr->execSqlSync(
-          "select repository, sha256sum from package where name = $1 and version = $2 limit 1",
-          name.asString(), version.asString()
-      );
-
-      Json::Value packages(Json::arrayValue);
-      for (const drogon::orm::Row& row : result) {
-        Json::Value package(Json::objectValue);
-        package["repository"] = row["repository"].as<std::string>();
-        package["sha256sum"] = row["sha256sum"].as<std::string>();
-        packages.append(package);
-      }
-      callback(poac_api::ok(packages));
-    } catch (const drogon::orm::DrogonDbException& e) {
-      LOG_ERROR << e.base().what();
-      callback(poac_api::internalError());
-    }
-  } else {
+  if (!(name.isString() && version.isString())) {
     callback(poac_api::badRequest("`name` & `version` must be string"));
+    return;
+  }
+
+  // SQL query
+  const drogon::orm::DbClientPtr clientPtr = drogon::app().getDbClient();
+  try {
+    const auto result = clientPtr->execSqlSync(
+        "select repository, sha256sum from package where name = $1 and version = $2 limit 1",
+        name.asString(), version.asString()
+    );
+
+    // Response build
+    Json::Value packages(Json::arrayValue);
+    for (const drogon::orm::Row& row : result) {
+      Json::Value package(Json::objectValue);
+      package["repository"] = row["repository"].as<std::string>();
+      package["sha256sum"] = row["sha256sum"].as<std::string>();
+      packages.append(package);
+    }
+    callback(poac_api::ok(packages));
+  } catch (const drogon::orm::DrogonDbException& e) {
+    LOG_ERROR << e.base().what();
+    callback(poac_api::internalError());
   }
 }
