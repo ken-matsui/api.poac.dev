58566db9efeb60037747b4cddb3691152b15a5fa
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-30 03:06:21 +0900

    Create migrations/package.sql

---
+++ migrations/20220517162513_package.sql
@@ -0,0 +1,16 @@
+create table public.package (
+    id uuid primary key default uuid_generate_v4(),
+    published_at timestamptz not null default now(),
+    metadata jsonb not null unique,
+    name text not null,
+    version text not null,
+    edition int2 not null,
+    authors text[] not null,
+    repository text not null,
+    description text not null,
+    license text not null,
+    readme text, -- has markdown
+    sha256sum varchar(64) not null,
+
+    unique (name, version)
+);
