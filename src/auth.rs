009edbb956cde15a9aececbbada1e3ad2ec73a02
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-30 20:25:45 +0900

    Implement more authCallback

--- src/auth.rs
+++ src/auth.rs
@@ -10,9 +10,10 @@ use get_access_token::get_access_token;
 use get_email::get_email;
 use get_user_meta::get_user_meta;
 
-use actix_web::error::ErrorInternalServerError;
+use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
 use actix_web::{get, web, HttpResponse, Result};
-use poac_api_utils::{DbPool, Response};
+use diesel::prelude::*;
+use poac_api_utils::{DbError, DbPool, Response};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -37,23 +38,54 @@ async fn auth_callback(
     .await?
     .map_err(ErrorInternalServerError)?;
 
-    match user {
+    let user = match user {
         None => {
             // Create a new user so that there was not the same user.
             let email = get_email(&access_token).await?;
-            let user = web::block(move || {
-                let mut conn = pool.get()?;
-                create_user(&mut conn, &user_meta, &email)
+
+            let pool_cloned = pool.clone();
+            let user_meta_cloned = user_meta.clone();
+            web::block(move || {
+                let mut conn = pool_cloned.get()?;
+                create_user(&mut conn, &user_meta_cloned, &email)
             })
             .await?
-            .map_err(ErrorInternalServerError)?;
-            Ok(Response::ok(user))
+            .map_err(ErrorInternalServerError)?
         }
         Some(user) => {
             // This is NOT a new user
-            Err(ErrorInternalServerError(""))
+            if user.status != "active" {
+                log::warn!("A disabled user tried to log in: {:?}", user);
+                return Err(ErrorUnauthorized("You are not authorized."));
+            }
+
+            // Stale user info (user_meta must be up-to-date)
+            if user_meta.name != user.name || user_meta.avatar_url != user.avatar_url {
+                web::block(move || -> Result<crate::user::models::User, DbError> {
+                    use crate::schema::users::dsl::{avatar_url, id, name, user_name, users};
+
+                    let mut conn = pool.get()?;
+                    let user = diesel::update(users)
+                        .filter(user_name.eq(user_meta.user_name))
+                        .set((
+                            name.eq(&user_meta.name),
+                            avatar_url.eq(&user_meta.avatar_url),
+                        ))
+                        .returning((id, name, user_name, avatar_url))
+                        .get_result::<crate::user::models::User>(&mut conn)?;
+
+                    Ok(user)
+                })
+                .await?
+                .map_err(ErrorInternalServerError)?
+            } else {
+                crate::user::models::User::from(user)
+            }
         }
-    }
+    };
+
+    // TODO: Convert user to base64
+    Ok(Response::ok(user))
 }
 
 pub(crate) fn init_routes(cfg: &mut web::ServiceConfig) {
--- src/auth/find_user.rs
+++ src/auth/find_user.rs
@@ -3,13 +3,26 @@ use diesel::prelude::*;
 use diesel::PgConnection;
 use poac_api_utils::{log_query, DbError};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Queryable, Serialize, Deserialize)]
+#[derive(Queryable, Serialize, Deserialize, Debug)]
 pub(crate) struct User {
-    name: String,
+    id: Uuid,
+    pub(crate) name: String,
     user_name: String,
-    avatar_url: String,
-    status: String,
+    pub(crate) avatar_url: String,
+    pub(crate) status: String,
+}
+
+impl From<User> for crate::user::models::User {
+    fn from(user: User) -> Self {
+        Self {
+            id: user.id,
+            name: user.name,
+            user_name: user.user_name,
+            avatar_url: user.avatar_url,
+        }
+    }
 }
 
 pub(crate) fn find_user(
@@ -19,7 +32,7 @@ pub(crate) fn find_user(
     use crate::schema::users::dsl::*;
 
     let query = users
-        .select((name, user_name, avatar_url, status))
+        .select((id, name, user_name, avatar_url, status))
         .filter(user_name.eq(user_meta.user_name));
     log_query(&query);
 
