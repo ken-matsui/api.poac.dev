71ee6b2e8ee22a2fb0c107d8e70c78e1bead6dab
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-30 09:20:35 +0900

    Remove unnecessary codes

--- controllers/v1.h
+++ controllers/v1.h
@@ -1,13 +1,5 @@
 #pragma once
 
-// std
-#include <string>
-
-// external
-#include <drogon/HttpController.h> // NOLINT(build/include_order)
-
-using namespace drogon;
-
 class v1 : public drogon::HttpController<v1> {
 public:
   METHOD_LIST_BEGIN
@@ -27,61 +19,4 @@ public:
   METHOD_ADD(v1::versions, "/versions", Post, "JsonReq");
 
   METHOD_LIST_END
-
-  static void
-  authCallback(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback
-  );
-
-  static void
-  owners(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback,
-      const std::string& name
-  );
-  static void
-  ownersOrg(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback,
-      const std::string& org,
-      const std::string& name
-  );
-
-  static void
-  dependents(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback,
-      const std::string& name
-  );
-  static void
-  dependentsOrg(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback,
-      const std::string& org,
-      const std::string& name
-  );
-
-  static void
-  userPackages(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback,
-      const std::string& userName
-  );
-
-  static void
-  deps(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback
-  );
-  static void
-  repoinfo(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback
-  );
-  static void
-  versions(
-      const HttpRequestPtr& req,
-      std::function<void(const HttpResponsePtr&)>&& callback
-  );
 };
