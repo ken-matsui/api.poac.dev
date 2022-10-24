b5e910477fed70bf1d154b744276593cffa73ed8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-10-24 15:17:59 -0700

    Split healthcheck

---
+++ src/health.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, HttpResponse};
+use diesel::dsl::sql;
+use diesel::sql_types::Bool;
+use diesel::RunQueryDsl;
+use poac_api_utils::DbPool;
+
+pub(crate) async fn status(pool: web::Data<DbPool>) -> HttpResponse {
+    let is_database_connected = web::block(move || {
+        if let Ok(mut conn) = pool.get() {
+            sql::<Bool>("SELECT 1").execute(&mut conn).is_ok()
+        } else {
+            false
+        }
+    })
+    .await
+    .unwrap_or(false);
+
+    let db_status = serde_json::json!({ "database_connected": is_database_connected }).to_string();
+    if is_database_connected {
+        HttpResponse::Ok()
+            .content_type("application/json")
+            .body(db_status)
+    } else {
+        HttpResponse::ServiceUnavailable()
+            .content_type("application/json")
+            .body(db_status)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,15 @@
 mod auth;
+mod health;
 mod package;
 mod routes;
 mod schema;
 mod user;
 
-use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
-use diesel::dsl::sql;
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use diesel::r2d2::{ConnectionManager, Pool};
-use diesel::sql_types::Bool;
-use diesel::{PgConnection, RunQueryDsl};
+use diesel::PgConnection;
 use dotenvy::dotenv;
-use poac_api_utils::DbPool;
 use std::env;
-
-async fn get_health_status(pool: web::Data<DbPool>) -> HttpResponse {
-    let is_database_connected = web::block(move || {
-        if let Ok(mut conn) = pool.get() {
-            sql::<Bool>("SELECT 1").execute(&mut conn).is_ok()
-        } else {
-            false
-        }
-    })
-    .await
-    .unwrap_or(false);
-
-    let db_status = serde_json::json!({ "database_connected": is_database_connected }).to_string();
-    if is_database_connected {
-        HttpResponse::Ok()
-            .content_type("application/json")
-            .body(db_status)
-    } else {
-        HttpResponse::ServiceUnavailable()
-            .content_type("application/json")
-            .body(db_status)
-    }
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -58,7 +33,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // Set up DB pool to be used with web::Data<Pool> extractor
             .app_data(web::Data::new(pool.clone()))
-            .route("/health", web::get().to(get_health_status))
+            .route("/health", web::get().to(health::status))
             .wrap(Logger::default())
             .configure(routes::init_routes)
     })
