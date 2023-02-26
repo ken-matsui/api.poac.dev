01f615a79a9a9687c6dfeaa70424e60065f436e6
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-26 05:01:12 -0800

    Split user/actions

--- src/user/actions.rs
+++ src/user/actions.rs
@@ -1,19 +1,3 @@
-use crate::user::models::User;
-use diesel::prelude::*;
-use diesel::PgConnection;
-use poac_api_utils::{log_query, DbError};
+mod owners;
 
-pub(crate) fn owners(conn: &mut PgConnection, name_: &str) -> Result<Vec<User>, DbError> {
-    use crate::schema::ownerships::dsl::{ownerships, package_name, user_id};
-    use crate::schema::users::dsl::*;
-
-    let query = users
-        .select((id, name, user_name, avatar_url))
-        .left_join(ownerships.on(package_name.eq(name_)))
-        .filter(user_id.eq(id))
-        .filter(status.eq("active"));
-    log_query(&query);
-
-    let results = query.load::<User>(conn)?;
-    Ok(results)
-}
+pub(crate) use owners::owners;
