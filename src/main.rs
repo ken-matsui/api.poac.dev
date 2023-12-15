6b54a83b526560ccb0dee38f94af58ab8c4c9933
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-12-14 19:54:46 -0800

    Migration to Shuttle

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,22 @@ use std::env;
 use actix_web::{middleware::Logger, web, web::ServiceConfig};
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
-use dotenvy::dotenv;
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_secrets::SecretStore;
 
 #[shuttle_runtime::main]
-async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    dotenv().ok();
+async fn actix_web(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    // Set up secrets
+    let database_url = secret_store.get("DATABASE_URL").expect("DATABASE_URL must be set");
+    let gh_oauth_client_id = secret_store.get("GITHUB_OAUTH_CLIENT_ID").expect("GITHUB_OAUTH_CLIENT_ID must be set");
+    let gh_oauth_client_secret = secret_store.get("GITHUB_OAUTH_CLIENT_SECRET").expect("GITHUB_OAUTH_CLIENT_SECRET must be set");
+
+    env::set_var("GITHUB_OAUTH_CLIENT_ID", gh_oauth_client_id);
+    env::set_var("GITHUB_OAUTH_CLIENT_SECRET", gh_oauth_client_secret);
 
     // Set up database connection pool
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = Pool::builder()
         .build(manager)
